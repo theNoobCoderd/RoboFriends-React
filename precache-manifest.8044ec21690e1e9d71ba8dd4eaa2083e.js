718ccc099b67a225c47ac9b412dba45bc21b700e
ismailramjean <ismailramjean@gmail.com>
2020-08-28 11:16:29 +0400

    Updates

---
+++ precache-manifest.8044ec21690e1e9d71ba8dd4eaa2083e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4b9c1b49ba044a0cc59e190181cd81",
+    "url": "/RoboFriends-React/index.html"
+  },
+  {
+    "revision": "00c63d96483b440f90bf",
+    "url": "/RoboFriends-React/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d2e93a2105ab292d3836",
+    "url": "/RoboFriends-React/static/css/main.1171fee7.chunk.css"
+  },
+  {
+    "revision": "00c63d96483b440f90bf",
+    "url": "/RoboFriends-React/static/js/2.076b1152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends-React/static/js/2.076b1152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e93a2105ab292d3836",
+    "url": "/RoboFriends-React/static/js/main.2d2e7b10.chunk.js"
+  },
+  {
+    "revision": "cb17d6b59b4fe2efa2eb",
+    "url": "/RoboFriends-React/static/js/runtime-main.59dfcb75.js"
+  }
+]);
